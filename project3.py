688dd0372853c60fff4a83b202a5443133c2e530
Harshvardhan Shaktawat <178646935+luv524@users.noreply.github.com>
2025-03-24 17:50:52 +0530

    Added Madibs python code

---
+++ project3.py
@@ -0,0 +1,14 @@
+#madlibs game
+#it is a type of game in which you fill  spaces with random words dude.
+name = input("enter your name: ")
+adjective1 = input("enter a adjective like an emotion or something like that: ")
+verb = input("enter a noun, place, thing, etc: ")
+adjective2 = input("enter a adjective like an emotion or something like that, this will be another thing dude: ")
+adjective3 = input("enter a adjective like an emotion or something like that: ")
+
+print(f"hello my name is{name} \n")
+print(f"i went to {adjective1} for some reason \n")
+print(f"that place was nice and cool and was {verb}\n the place was fabolus\n\t") 
+print(f"i {adjective2} the place \n")
+print(f"there was my favourite {adjective3} \n")
+print("the journey was faboulus in the end !!")

8013a62b3a93a63d533ab8e7a87bad6f553748bc
Harshvardhan Shaktawat <178646935+luv524@users.noreply.github.com>
2025-09-01 22:50:43 +0530

    Add files via upload
    so this is the calculator program simply made by using python

---
+++ calculator.py
@@ -0,0 +1,17 @@
+#so some new codes writing ehre now thsu bro 
+# so now here we are making a calculaotr program here bro
+operator = input("enter the operator you want to use \n +,-,*,/: ")
+a =  int(input("enter the number: "))
+b = int(input("enter another number: "))
+if operator == "+":
+    print (a +b)
+elif operator == "-":
+    print(a-b)
+elif operator == "*":
+    print(a*b)
+elif operator == "/":
+    print(a/b)
+else:
+    print("please enter a valid operator buddy")
+# so this is the calcultor program which is quite simple and easy to use thus bro,
+# it also looks quite simple and good to use thus bro 

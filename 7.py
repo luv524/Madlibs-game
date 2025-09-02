b549432e3586d98ccbcacd6a1fddc8c5c0231551
Harshvardhan Shaktawat <178646935+luv524@users.noreply.github.com>
2025-09-02 20:19:32 +0530

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,14 @@
+#so now i am writing a iterative funciton here then bro 
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+        return result
+print(factorial_iterative(7))
+
+
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n+1):
+        return factorial_iterative
+    print(factorial_iterative(5))
---
+++ 8.py
@@ -0,0 +1,12 @@
+# so hello 
+s = "1"
+print(bool(s))   
+
+s = ""
+print(bool(s))  
+
+s = "hello"
+print(bool(s)) #so this is true becasue it is non- empty string so it is true then.
+s = ""
+print(bool(s))  # it will be false as this one is empty string and anything that is empty it will be false 
+    # alwyas then bro.
